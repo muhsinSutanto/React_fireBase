5dc1b0d7deb3a162757d1695c6c708da778702b7
monggopesen 4 <sutantomuhsin@gmai.com>
2019-02-27 12:38:18 +0700

    sum up balance

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -4,11 +4,29 @@ import PropTypes from 'prop-types';
 import {compose} from 'redux'
 import {connect} from 'react-redux'
 import {firestoreConnect} from 'react-redux-firebase'
+import Spinner from '../layout/Spinner'
 
 class Clients extends Component {
+    state = {
+        totalOwed: null
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        const {clients} = props
+
+        if(clients) {
+            //add balance
+            const Total = clients.reduce((total, client) => {
+                return total+ parseFloat(client.balance.toString())
+            },0)
+            return {totalOwed: Total}
+        }
+    }   
+    
 
     render() {
         const {clients} = this.props
+        const {totalOwed} = this.state
         
         if(clients){
             return ( 
@@ -17,7 +35,13 @@ class Clients extends Component {
                         <div className='col-md 6'>
                             <h2> <i className='fas fa-users'> Clients</i></h2>
                         </div>
-                        <div className='col-md-6'/>
+                        <div className='col-md-6'>
+                            <h5 className='text-right text-secondary'> Total Owed
+                                <span className='text-primary'> 
+                                    ${parseFloat(totalOwed).toFixed(2)}
+                                </span>
+                            </h5>
+                        </div>
                     </div>
                         <table className='table table-striped'> 
                             <thead className='thed-inverse'> 
@@ -47,7 +71,7 @@ class Clients extends Component {
                 </div>
              );
         } else {
-            return <h1>Loading</h1>
+            return <Spinner />
         }
         
     }
